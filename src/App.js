aff5e6f98d63db811040c18f0807ebe5cd61659c
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-24 17:38:40 -0500

    Add store provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
 
