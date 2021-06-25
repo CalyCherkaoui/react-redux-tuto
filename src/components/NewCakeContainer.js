8b4aa52da7c0263ab291ef05f412066a64366879
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-25 13:00:51 -0500

    Add newCake component to buy a num of cakes

---
+++ src/components/NewCakeContainer.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { buyCake } from '../redux'
+
+function NewCakeContainer(props) {
+  const [number, setNumber] = useState(1)
+  return (
+    <div>
+      <hr></hr>
+      <p><em>New Cakes container</em></p>
+      <h2>Number of cakes : {props.numOfCakes}</h2>
+      <input 
+        type="text"
+        value={number}
+        onChange={e => setNumber(e.target.value)}
+      />
+      <button
+        onClick={() => props.buyCake(number)}
+      >
+        Click to buy those cakes
+      </button>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    numOfCakes: state.cake.numOfCakes
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    buyCake: (number) => dispatch(buyCake(number))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+  )(NewCakeContainer)
