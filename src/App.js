08271b726d0bb12f670f8ac2e9a36c2c5cf7678f
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-25 11:04:53 -0500

    Add hooksCakeContainer component to App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <CakeContainer />
+        <HooksCakeContainer />
       </div>
     </Provider>
   );
