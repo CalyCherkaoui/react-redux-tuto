e0fa6e78bcffb0ca2ef0cee3064e9ce21beff4e0
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-25 11:00:30 -0500

    Implement useSelector hook

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function HooksCakeContainer() {
+  const numOfCakes = useSelector(state => state.numOfCakes)
   return (
     <div>
       <hr></hr>
       <p><em>Redux with Hooks</em></p>
-      <h2>Num of cakes : </h2>
+      <h2>Num of cakes : {numOfCakes}</h2>
       <button>Buy a cake</button>
     </div>
   )
