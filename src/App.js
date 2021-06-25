b64f5262336561e159908bc9261364d9dfc1ff17
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-25 15:18:09 -0500

    Add userContainer component to App

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
 import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <NewCakeContainer />
         <ItemContainer cake />
         <ItemContainer />
+        <UserContainer />
       </div>
     </Provider>
   );
--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -4,15 +4,32 @@ import { connect } from 'react-redux'
 import { fetchUsers } from '../redux'
 
 
-function UserContainer( { fetchUsers } ) { // props destructured == props.fetchUsers
+function UserContainer( { userData, fetchUsers } ) { // props destructured == props.fetchUsers
   useEffect(
     () => { fetchUsers()}
   , []) // empty dependencies array so that fetchUsers is dispatched only once
-  return (
-    <div>
-      
-    </div>
-  )
+  return userData.loading ? (
+      <div>
+        <hr></hr>
+        <p><em>Async - request</em></p>
+        <h2>loading data from api .......</h2>
+      </div>
+    ) : userData.error ? (
+      <h2>{userData.error}</h2>
+    ) : (
+      <div>
+        <hr></hr>
+        <p><em>Async - success</em></p>
+        <h2>Users list</h2>
+        <ul>
+          {
+            userData &&
+            userData.users &&
+            userData.users.map(user => <li>{user.name}</li>)
+          }
+        </ul>
+      </div>
+    )
 }
 
 const mapStateToProps = state => {
