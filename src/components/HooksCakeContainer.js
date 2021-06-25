944d46636c4b2aef9fada5a699d0097a91bad5b8
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-25 10:57:11 -0500

    Add HooksCakeContainer component

---
+++ src/components/HooksCakeContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function HooksCakeContainer() {
+  return (
+    <div>
+      <hr></hr>
+      <p><em>Redux with Hooks</em></p>
+      <h2>Num of cakes : </h2>
+      <button>Buy a cake</button>
+    </div>
+  )
+}
+
+export default HooksCakeContainer
+
