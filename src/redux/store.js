c0179dd56225a5c568a8b42c2410a70b16ccb531
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-25 14:35:05 -0500

    Add thunk middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 // import cakeReducer from "./cake/cakeReducer";
 import logger from "redux-logger";
 import rootRducer from "./rootReducer";
 
-const store = createStore(rootRducer, applyMiddleware(logger));
+const store = createStore(rootRducer, applyMiddleware(logger, thunk));
 
 export default store
